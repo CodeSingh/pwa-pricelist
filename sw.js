7263fa32128755592b70965dfe48e472dcc2117a
CodeSingh <sugi007@yahoo.com>
2018-11-11 09:52:29 +0000

    service worker added fetch statement

--- sw.js
+++ sw.js
@@ -14,3 +14,11 @@ self.addEventListener('install', function(e) {
    })
  );
 });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || new Response("Nothing in the cache for this request");
+      })
+    );
+  });
