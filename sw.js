562c80d85664b4bd983998fa2817bdd66f90327c
CodeSingh <sugi007@yahoo.com>
2018-11-11 09:49:01 +0000

    services worker cache

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
-// Test
+//importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('pwa-lricesheet').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.css',
+       '/manifest.json',
+       '/PriceList.png',
+       '/vue.js'
+     ]);
+   })
+ );
+});
