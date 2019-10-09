65330ec4e5d43bbba82cf0a0a5a96fa0cabba4ae
CodeSingh <sugi007@yahoo.com>
2019-10-09 20:23:39 +0100

    Changed version to force refresh

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var GENERAL_CACHE_NAME = 'pwa-pricesheet'
-var CACHE_NAME = GENERAL_CACHE_NAME + 'v15';
+var CACHE_NAME = GENERAL_CACHE_NAME + 'v16';
 
 self.addEventListener('install', function(e) {
  e.waitUntil(
